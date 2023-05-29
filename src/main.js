605f9c50636ee1db755dfd6ff403c1036bc2165e
viamazoba <viamazoba@unal.edu.co>
2023-05-28 20:35:37 -0500

    Se establecen las tendencias y las categorias de la página

--- src/main.js
+++ src/main.js
@@ -1,1 +1,51 @@
-console.log('Está funcionando el script!')
+async function getTrendingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+
+    const data = await res.json();
+
+    const movies = data.results;
+    
+    movies.forEach(movie => {
+        const trendingSection = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt',movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path)
+
+        movieContainer.appendChild(movieImg)
+        trendingSection.appendChild(movieContainer)
+    });
+
+    
+}
+
+async function getCategoriesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+
+    const data = await res.json();
+
+    const categories = data.genres;
+    
+    categories.forEach(category => {
+        const categorySection = document.querySelector('#categoriesPreview .categoriesPreview-list');
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id','id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+        categoryTitle.appendChild(categoryTitleText);
+
+        categoryContainer.appendChild(categoryTitle)
+        categorySection.appendChild(categoryContainer)
+    });
+
+    
+}
+
+
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
