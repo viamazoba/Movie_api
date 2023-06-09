2ba67fc1e581ff4359fb8ac7c3b94277315239a1
viamazoba <viamazoba@unal.edu.co>
2023-06-09 08:11:57 -0500

    Se crea función de navegación en el documento navigation.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-async function getTrendingMoviesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
 
-    const data = await res.json();
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+});
+
+async function getTrendingMoviesPreview(){
+    const {data} = await api('trending/movie/day');
 
     const movies = data.results;
     
@@ -22,9 +31,7 @@ async function getTrendingMoviesPreview(){
 }
 
 async function getCategoriesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-
-    const data = await res.json();
+    const {data} = await api('genre/movie/list');
 
     const categories = data.genres;
     
@@ -44,8 +51,3 @@ async function getCategoriesPreview(){
 
     
 }
-
-
-
-getTrendingMoviesPreview();
-getCategoriesPreview();
